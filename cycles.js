9cf01fdda9ca84aaff8108f5fe3c8aad34de98bf
rameshhb37 <rameshhb37@gmail.com>
2024-02-24 23:27:30 +0530

    feat: Added some coding problems

---
+++ cycles.js
@@ -0,0 +1,17 @@
+// Implement a function that takes one or more values and returns a function that cycles through those values each time it is called
+
+function cycle(input) {
+    const inputArr = input.split(',');
+    let index = 0;
+
+    return function() {
+        const currentVal = inputArr[index];
+        index = (index + 1) % inputArr.length;
+        return currentVal;
+    }
+}
+
+const test = cycle('hello, hi');
+console.log(test());
+console.log(test());
+console.log(test());
---
+++ type-check-utility.js
@@ -0,0 +1,9 @@
+// Implement utilities to determine primitive variable types in JavaScript
+
+function primitiveTypeCheck(input) {
+	const type = typeof input;
+	if (type !== "object") {
+		return true;
+	}
+	return false;
+}
