2f6ca8d4ed9c4bfc74e1b21d2a6f13e164cb1afa
rameshhb37 <rameshhb37@gmail.com>
2024-03-06 16:15:25 +0530

    feat: Added number line jumps problem

---
+++ find-the-missing-number-in-array.js
@@ -0,0 +1,13 @@
+// Given an array containing numbers from 1 to N, with one number missing, find the missing number.
+
+function missingNumberInAnArray(arr) {
+	const N = arr.length + 1;
+	const expectedSum = (N * (N + 1)) / 2;
+
+	const actualSum = arr.reduce((acc, next) => acc + next, 0);
+	const missingNumber = expectedSum - actualSum;
+
+	return missingNumber;
+}
+
+console.log(missingNumberInAnArray([5, 2, 3, 1]));
---
+++ number-line-jumps.js
@@ -0,0 +1,19 @@
+//  "Number Line Jumps" or "Kangaroo Jumping" problem,
+
+function kangarooJump(x1, v1, x2, v2) {
+	if (x1 === x2 && v1 === v2) {
+		return "YES";
+	}
+
+	if (v1 === v2) {
+		return "NO";
+	}
+
+	if ((x2 - x1) % (v1 - v2) == 0 && (x2 - x1) / (v1 - v2) >= 0) {
+		return "YES";
+	}
+
+	return "NO";
+}
+
+console.log(kangarooJump(0, 2, 5, 3));
